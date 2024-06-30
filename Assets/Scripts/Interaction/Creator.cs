3fad46a43f4f0e583c84462d6298d3cd52935f83
Алексей Макаров <33080157+theokunb@users.noreply.github.com>
2024-06-30 16:47:37 +0900

    hot fix

--- Assets/Scripts/Interaction/Creator.cs
+++ Assets/Scripts/Interaction/Creator.cs
@@ -18,9 +18,19 @@ public abstract class Creator<T> : IService, IWindow where T : Component
     public T CreateInteraction(GameObject prefab)
     {
         var instance = UnityEngine.Object.Instantiate(prefab, _canvas.transform);
+        instance.transform.SetSiblingIndex(1);
 
-        EnterMoveAnimation(instance.transform);
-        EnterFadeAnimation();
+        instance.transform.position -= new Vector3(0, Screen.height, 0);
+        float targetPostion = instance.transform.position.y + Screen.height;
+
+        instance.transform.DOMoveY(targetPostion, 0.7f).OnComplete(() =>
+        {
+            AfterCreate();
+        });
+
+        var mainCanvasGroup = ServiceLoacator.Instance.Get<GlobalCanvasGroup>();
+        mainCanvasGroup.CanvasGroup.alpha = 0;
+        mainCanvasGroup.CanvasGroup.DOFade(0.65f, 0.7f);
 
         var player = ServiceLoacator.Instance.Get<Player>();
         player.enabled = false;
@@ -32,59 +42,23 @@ public abstract class Creator<T> : IService, IWindow where T : Component
         return instance.GetComponent<T>();
     }
 
-    protected virtual void EnterMoveAnimation(Transform transform)
-    {
-        transform.position -= new Vector3(0,Screen.height,0);
-        float targetPostion = transform.position.y + Screen.height;
-
-        transform.DOMoveY(targetPostion, 0.7f).OnComplete(() =>
-        {
-            AfterCreate();
-        });
-    }
-
-    protected virtual void EnterFadeAnimation()
-    {
-        var mainCanvasGroup = ServiceLoacator.Instance.Get<GlobalCanvasGroup>();
-        mainCanvasGroup.CanvasGroup.alpha = 0;
-        mainCanvasGroup.CanvasGroup.DOFade(0.65f, 0.7f);
-    }
-
     public void HideInteraction()
     {
         var player = ServiceLoacator.Instance.Get<Player>();
         player.enabled = true;
         Closing?.Invoke();
 
-        OutFadeAnimation();
-        OnHiding();
-        OutMoveAnimation();
-    }
-
-    protected virtual void OutFadeAnimation()
-    {
         var mainCanvasGroup = ServiceLoacator.Instance.Get<GlobalCanvasGroup>();
         mainCanvasGroup.CanvasGroup.alpha = 0.65f;
         mainCanvasGroup.CanvasGroup.DOFade(0f, 0.5f);
-    }
-
-    /// <summary>
-    /// если вы переопределяете данный метод обязательно вызовите метод DestroyInteraction()
-    /// </summary>
-    protected virtual void OutMoveAnimation()
-    {
+        OnHiding();
         CurrentInteraction?.transform.DOMoveY(-Screen.height, 0.7f).OnComplete(() =>
         {
-            DestroyInteraction();
+            var interactionService = ServiceLoacator.Instance.Get<InteractionService>();
+            interactionService.Free();
+
+            UnityEngine.Object.Destroy(CurrentInteraction.gameObject);
         });
-    }
-
-    protected void DestroyInteraction()
-    {
-        var interactionService = ServiceLoacator.Instance.Get<InteractionService>();
-        interactionService.Free();
-
-        UnityEngine.Object.Destroy(CurrentInteraction.gameObject);
     }
 
     /// <summary>
